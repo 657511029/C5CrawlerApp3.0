80df8d8d472ce1ef2d569c928c0fa6a86daa48aa
黄一雄 <yixiong.huang@delonixmail.com>
2024-01-12 16:21:33 +0800

    Initialize Project

---
+++ C5CrawlerServer/src/main/java/com/example/c5crawlerserver/C5CrawlerServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.c5crawlerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C5CrawlerServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(C5CrawlerServerApplication.class, args);
+    }
+
+}
---
+++ C5CrawlerServer/src/test/java/com/example/c5crawlerserver/C5CrawlerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.c5crawlerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C5CrawlerServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
